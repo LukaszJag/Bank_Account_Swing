bbc80a19bfd892124a54790b2a376d21fe6c0656
LukaszJag <lukasz.jagnow@gmail.com>
2022-06-29 12:22:19 +0200

    Add simple main gui

--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -16,21 +16,52 @@ public class MainWindow extends JFrame{
     private JLabel bankInformationsTotalMoney;
     private JButton acctepButton;
 
+    private BorderLayout startWindowBorderLayout;
 
     public MainWindow(){
         makeWindow();
     }
 
+    private JPanel panel1 = new JPanel();
+    private JPanel panel2 = new JPanel();
+    private JPanel panel3 = new JPanel();
+    private JPanel panel4 = new JPanel();
+    private JPanel panel5 = new JPanel();
 
     public void makeWindow(){
 
         JOptionPane.showMessageDialog(this,"Welcome in Bank Application");
+
         startWindow = new JFrame("Bank Application");
         startWindow.setSize(Config.START_WINDOWS_WIDTH, Config.START_WINDOWS_HEIGHT);
+        startWindowBorderLayout = new BorderLayout();
+        startWindow.setLayout(startWindowBorderLayout);
+
+
+
+        panel1.setBackground(Color.BLACK);
+        panel2.setBackground(Color.GRAY);
+        panel3.setBackground(Color.DARK_GRAY);
+        panel4.setBackground(Color.BLUE);
+        panel5.setBackground(Color.WHITE);
+
+        panel1.setPreferredSize(new Dimension(100,40));
+        panel2.setPreferredSize(new Dimension(150,100));
+        panel3.setPreferredSize(new Dimension(100,100));
+        panel4.setPreferredSize(new Dimension(100,100));
+        panel5.setPreferredSize(new Dimension(100,25));
 
         buildPanel();
 
-        startWindow.add(startWindowPanel);
+        startWindow.add(panel1, BorderLayout.NORTH);
+        startWindow.add(panel2, BorderLayout.WEST);
+        startWindow.add(panel3, BorderLayout.CENTER);
+        startWindow.add(panel4, BorderLayout.EAST);
+        startWindow.add(panel5, BorderLayout.SOUTH);
+
+
+
+
         startWindow.setLocationRelativeTo(null);
         startWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         startWindow.setResizable(false);
@@ -42,19 +73,17 @@ public class MainWindow extends JFrame{
 
         startWindowPanel = new JPanel();
 
-        bankInformations = new JLabel("Bank app manager.");
-        bankInformationsUsers = new JLabel("Active users: " + Config.users);
-        bankInformationsTotalMoney = new JLabel("Total money in bank: " + Config.totalMoney);
-        askingLabel = new JLabel("What action do you want to do?");
-        optionComboBox = new JComboBox<>(Config.options);
-        acctepButton = new JButton("Ok");
+        Button openAccountButton = new Button("Open new bank acoount");
+        Button depositButton = new Button("Deposit money");
+        Button withdrawButton = new Button("Withdraw money");
+        Button closeAccountButton = new Button("Close bank account");
 
-        startWindowPanel.add(bankInformations);
-        startWindowPanel.add(bankInformationsUsers);
-        startWindowPanel.add(bankInformationsTotalMoney);
-        startWindowPanel.add(askingLabel);
-        startWindowPanel.add(optionComboBox);
-        startWindowPanel.add(acctepButton);
+
+
+        panel2.add(openAccountButton);
+        panel2.add(depositButton);
+        panel2.add(withdrawButton);
+        panel2.add(closeAccountButton);
 
     }
 }
