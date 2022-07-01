136a1190357c3e6fca2a254130dc9fbbff28589b
LukaszJag <lukasz.jagnow@gmail.com>
2022-07-01 13:26:02 +0200

    clean up code
    delete unused methods

--- src/fileOperations/BasicFilesOperations.java
+++ src/fileOperations/BasicFilesOperations.java
@@ -13,26 +13,7 @@ public class BasicFilesOperations {
     private static String fileName = "accountsData.txt";
     private static String fullFilePathAndName = fileLocation + fileName;
 
-    public void readData() {
-        int line = 0;
-        String currentLine = "";
-
-        try {
-            File file = new File(fullFilePathAndName);
-            Scanner reader = new Scanner(file);
-
-            while (reader.hasNextLine()) {
-                line++;
-                currentLine = reader.nextLine();
-                System.out.println(currentLine);
-
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        }
-    }
-
-    public ArrayList readLines() {
+    public static ArrayList readAllLines() {
         ArrayList lines = new ArrayList();
         String line = "";
 
@@ -43,7 +24,6 @@ public class BasicFilesOperations {
             while (reader.hasNextLine()) {
 
                 line = reader.nextLine();
-                System.out.println(line);
                 lines.add(line);
 
             }
@@ -55,32 +35,6 @@ public class BasicFilesOperations {
 
     }
 
-    public static void writeToFile() {
-        String lineToFile;
-        Scanner input = new Scanner(System.in);
-
-        try {
-            java.io.FileWriter myWriter = new java.io.FileWriter(fullFilePathAndName, true);
-            System.out.println("Now you write in: " + fileName + " file");
-            System.out.println("To save and exit input: x");
-
-            lineToFile = input.nextLine();
-            System.out.println("First line:" + "\n" + lineToFile);
-            while (lineToFile.charAt(0) != 'x') {
-
-                myWriter.write(lineToFile + "\n");
-                lineToFile = input.nextLine();
-                System.out.println(lineToFile);
-            }
-
-            myWriter.close();
-            System.out.println("Successfully wrote to the file.");
-
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
 
     public static void writeToFile(String line) {
         String lineToFile;
--- src/textFiles/AccountFileHandler.java
+++ src/textFiles/AccountFileHandler.java
@@ -2,14 +2,12 @@ package textFiles;
 
 import fileOperations.BasicFilesOperations;
 
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class AccountFileHandler {
 
     private BasicFilesOperations filesOperations = new BasicFilesOperations();
-
-    public String getName(){
-
-        return "Name doesn't exist";
-    }
 
     public static void openAccount(String[] data){
         String lineToFile = "";
@@ -25,34 +23,52 @@ public class AccountFileHandler {
         BasicFilesOperations.writeToFile(lineToFile);
     }
 
-    public double getbalance(char typeOfMoney){
-
-
-        return -1;
-    }
 
     public static void deposit(String name, String lastName, double amount, String typeOfMoney )
     {
+        if(searchUser(name, lastName) == -1){
+            JOptionPane.showMessageDialog(null,"Account doesn't exist");
+        }else{
+            int line = searchUser(name, lastName);
+            System.out.println(line);
 
+        }
 
     }
 
-    public int searchUser(String name, String lastName){
+    public static int searchUser(String name, String lastName){
         int line = -1;
+        int counter = -1;
+        boolean nameFound = false;
+        boolean lastNameFound = false;
+        boolean getMatch = false;
+        String stringLine;
+        String[] arrayStringLine;
 
+        ArrayList allAccountText = BasicFilesOperations.readAllLines();
+        ArrayList onlyNameAndLastName;
+
+        for (int i = 0; i < allAccountText.size(); i++) {
+            counter++;
+
+            stringLine = (String) allAccountText.get(i);
+            arrayStringLine = stringLine.split("#",0);
+            if(arrayStringLine[0].equals(name)){
+                nameFound = true;
+            }
+
+            if(arrayStringLine[1].equals(lastName)){
+                lastNameFound = true;
+            }
+
+            if ((lastNameFound == true) &&(lastNameFound == true)){
+                getMatch = true;
+                line = counter;
+            }
+
+        }
 
         return line;
     }
-    public void withdraw(String name, String lastName, double amount, String typeOfMoney )
-    {
-       /*
-        find user and add withdraw to his account
-       */
-    }
-    public void setBalance(String name, String lastName, double amount, char typeOfMoney)
-    {
-        /*
-        find user and add set ballance to his account
-       */
-    }
+
 }
